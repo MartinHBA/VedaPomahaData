91f4365cc3e65352fd08e87bb8c50d972c0e75d0
MartinHBA <martin.huba@outlook.com>
2021-12-05 10:33:20 +0100

    resumable

--- CakarenOckovanie/cakaren.ps1
+++ CakarenOckovanie/cakaren.ps1
@@ -1,10 +1,35 @@
+## set directory for git command to be successful
 Set-Location "D:\OneDrive\git\IZA\covid19-data\Vaccination"
-$versions = git rev-list --all --reverse -- OpenData_Slovakia_Vaccination_Cakaren.csv
 
+## load version list that we already have csv for
+# $versionsLoad = Get-Content "D:\OneDrive\git\VedaPomahaData\CakarenOckovanie\versionlist.txt"
+
+## check if there are newer versions  
+## rework needed ## to check just newer than commit , some special git command)
+$versionsCheck = git rev-list --all --reverse -- OpenData_Slovakia_Vaccination_Cakaren.csv
+
+## collection of newer csv
+$collection = @()
+## check for each git version if we have it or not, if we don't have it in list let's add it to collection
+foreach ($item in $versionsCheck) {
+    $SEL = Select-String -Path "D:\OneDrive\git\VedaPomahaData\CakarenOckovanie\versionlist.txt" -Pattern $item
+
+    if (!$SEL) {
+        $collection+=$item
+}
+}
+$collection
+
+### useful clutter
+##  $versionsCheck | Out-File "D:\OneDrive\git\VedaPomahaData\CakarenOckovanie\versionlist.txt" -Force
 ### git rev-list --all --reverse --pretty=oneline -- OpenData_Slovakia_Vaccination_Cakaren.csv
 
-$i=0
-foreach ($item in $versions) {
+## determine last file name to follow naming convention
+$lastFileName = (Get-ChildItem "D:\OneDrive\git\VedaPomahaData\CakarenOckovanie\data\"| Sort-Object LastWriteTime | Select-Object -last 1).BaseName
+$lastFileNameInt = [int]$lastFileName
+
+$i=$lastFileNameInt
+foreach ($item in $collection) {
     $i++
     $filename = "$($i).csv"
     $uri = "https://github.com/Institut-Zdravotnych-Analyz/covid19-data/raw/$($item)/Vaccination/OpenData_Slovakia_Vaccination_Cakaren.csv"
@@ -14,6 +39,3 @@ foreach ($item in $versions) {
 }
 
 # Import-Csv -Path myData.csv
-
-
-
