076df5da53175e5f728c0ee782474762137c8830
MartinHBA <martin.huba@outlook.com>
2021-12-05 09:59:03 +0100

    cakaren

--- CakarenOckovanie/cakaren.ps1
+++ CakarenOckovanie/cakaren.ps1
@@ -1,11 +1,19 @@
 Set-Location "D:\OneDrive\git\IZA\covid19-data\Vaccination"
-$versions = git rev-list --all -- OpenData_Slovakia_Vaccination_Cakaren.csv
+$versions = git rev-list --all --reverse -- OpenData_Slovakia_Vaccination_Cakaren.csv
+
+### git rev-list --all --reverse --pretty=oneline -- OpenData_Slovakia_Vaccination_Cakaren.csv
 
 $i=0
 foreach ($item in $versions) {
     $i++
     $filename = "$($i).csv"
-    git show $item  | Out-File -FilePath "D:\OneDrive\git\VedaPomahaData\CakarenOckovanie\data\$($filename)"
+    $uri = "https://github.com/Institut-Zdravotnych-Analyz/covid19-data/raw/$($item)/Vaccination/OpenData_Slovakia_Vaccination_Cakaren.csv"
+    $result = Invoke-WebRequest -Uri $uri -Method GET
+    $result.Content | Out-File -FilePath "D:\OneDrive\git\VedaPomahaData\CakarenOckovanie\data\$($filename)"
+
 }
 
 # Import-Csv -Path myData.csv
+
+
+
