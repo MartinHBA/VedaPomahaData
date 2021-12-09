2e0f82b2c050b9dc57cea71e5f6737fe06ddc2b6
MartinHBA <martin.huba@outlook.com>
2021-12-09 08:53:28 +0100

    removing unnecessary files

--- CakarenOckovanie/Run.ps1
+++
@@ -1,8 +0,0 @@
-$env:Path += ";C:\Programs\Python\Python39";
-$env:PATHEXT += ";.py"; 
-$arg1 = 'D:\OneDrive\git\VedaPomahaData\CakarenOckovanie\PythonScript\calculateTrend.py' 
-$arg2 = "201" 
-python $arg1 $arg2
-
-
-# D:\OneDrive\git\VedaPomahaData\CakarenOckovanie\PythonScript\test2.py
