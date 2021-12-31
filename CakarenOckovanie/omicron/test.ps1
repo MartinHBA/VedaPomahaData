ae62b748a2769e22640ec15ea2a0ad27b5924fbb
MartinHBA <martin.huba@outlook.com>
2021-12-31 09:41:56 +0100

    Updated 2021-12-31 09:41

--- CakarenOckovanie/omicron/test.ps1
+++ CakarenOckovanie/omicron/test.ps1
@@ -1,3 +1,9 @@
+## all files are downloaded now push it to git repo (not IZA but own repo)
+Set-Location "D:\OneDrive\git\VedaPomahaData"
+git pull
+
+
+
 $uri = 'https://mendel3.bii.a-star.edu.sg/METHODS/corona/gamma/MUTATIONS/data/map_b11529.json'
 
 $result = Invoke-RestMethod -Method Get -Uri $uri 
@@ -18,3 +24,9 @@ foreach ($item in $result) {
 
 
 $finalCollection | export-Csv -Append -NoClobber -Force -Encoding utf8 -Path "D:\OneDrive\git\VedaPomahaData\CakarenOckovanie\omicron\omicron.csv"
+
+## git add
+git add .
+$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm"
+git commit -m "Updated $($timestamp)"
+git push
